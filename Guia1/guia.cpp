c3f6cf3bfd69e96c1d122d63f8f726d4aa764c87
ratadesanja <103476368+ratadesanja@users.noreply.github.com>
2022-05-01 00:22:44 -0300

    Added problem 7

--- Guia1/guia.cpp
+++ Guia1/guia.cpp
@@ -159,14 +159,30 @@ void problem_6()
     }
 }
 
+void problem_7()
+{
+    cout << endl << "Problema 7 - Aproximación de e" << endl;
+    float n;
+    cout << "Ingrese el Valor de n: ";
+    cin >> n;
+    
+    if (n >= 0 && n < 100000)
+    {
+        float e = pow(1 + 1/n, n);
+        float result = e;
+        cout << "Resultado: " << result << endl;
+    }
+}
+
 int main()
 {
     //system("cls");
-    problem_1();
-    problem_2();
-    problem_3();
-    problem_4();
-    problem_5();
-    problem_6();
+    // problem_1();
+    // problem_2();
+    // problem_3();
+    // problem_4();
+    // problem_5();
+    // problem_6();
+    problem_7();
     return 0;
 }
